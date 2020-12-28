6c04be8f0e47083b83dd00ba95923230f47e599f
龙玖伟 <longjiuwei@infosouth.com.cn>
2020-12-28 15:57:55 +0800

    feat(web_admin/src)博客列表 详情

---
+++ web_admin/src/components/Drawer/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import marked from 'marked'
+import { Drawer } from 'antd';
+
+interface DrawerBlogProps {
+    visible: boolean
+    context: string
+    title: string
+    onClose: () => void
+}
+
+const DrawerBlog: React.FC<DrawerBlogProps> = (props) => {
+    const { title, visible, onClose, context } = props
+    const HtmlData = marked(context, { gfm: true, xhtml: false })
+    return <Drawer title={title} placement="right" closable={false} onClose={onClose} visible={visible} width={450}>
+        <div dangerouslySetInnerHTML={{ __html: HtmlData }}></div>
+    </Drawer>
+}
+export default DrawerBlog
--- web_admin/src/views/blogList/index.tsx
+++ web_admin/src/views/blogList/index.tsx
@@ -1,56 +1,13 @@
 import React from "react";
 import { Input, Button, message, Table, Tag, Image, Pagination } from 'antd'
+import Drawer from '../../components/Drawer'
 import { httpGetSelectBlog } from '../../api/api'
 import moment from 'moment'
 import './index.scss';
-const columns = [
-  {
-    title: '标题',
-    dataIndex: 'title',
-    key: 'title',
-  },
-  {
-    title: '创建时间',
-    dataIndex: 'createDate',
-    key: 'createDate',
-  },
-  {
-    title: '内容',
-    dataIndex: 'content',
-    key: 'content',
-    render: () => <Button size="small" type='primary'>查看</Button>
-  },
-  {
-    title: '被喜欢次数',
-    key: 'likeCount',
-    dataIndex: 'likeCount',
-  },
-  {
-    title: '被查看的次数',
-    key: 'viewCount',
-    dataIndex: 'viewCount',
-  },
-  {
-    title: '是否展示',
-    key: 'show_blog',
-    dataIndex: 'show_blog',
-    render: (text: any) => <Tag>{text === 1 ? '显示' : '隐藏'}</Tag>
-  },
-  {
-    title: '字数',
-    key: 'number_words',
-    dataIndex: 'number_words'
-  },
-  {
-    title: "封面图",
-    key: 'imgUrl',
-    dataIndex: 'imgUrl',
-    render: (img: string) => <Image width={50} src={img} />
-  }
-];
+
 interface BlogData {
   imgUrl: string
-  context: string
+  content: string
   createDate: string
   likeCount: number
   number_words: number
@@ -66,21 +23,79 @@ interface IBlogListState {
   loading: boolean
   total: number
   title: string | undefined
+  blogDetailVisible: boolean
+  temporaryText: string
 }
 
 export default class BlogList extends React.Component<{}, IBlogListState> {
+  private columns = [
+    {
+      title: '标题',
+      dataIndex: 'title',
+      key: 'title',
+    },
+    {
+      title: '创建时间',
+      dataIndex: 'createDate',
+      key: 'createDate',
+    },
+    {
+      title: '被喜欢次数',
+      key: 'likeCount',
+      dataIndex: 'likeCount',
+    },
+    {
+      title: '被查看的次数',
+      key: 'viewCount',
+      dataIndex: 'viewCount',
+    },
+    {
+      title: '是否展示',
+      key: 'show_blog',
+      dataIndex: 'show_blog',
+      render: (text: any) => <Tag>{text === 1 ? '显示' : '隐藏'}</Tag>
+    },
+    {
+      title: '字数',
+      key: 'number_words',
+      dataIndex: 'number_words'
+    },
+    {
+      title: "封面图",
+      key: 'imgUrl',
+      dataIndex: 'imgUrl',
+      render: (img: string) => <Image width={50} src={img} />
+    },
+    {
+      title: "操作",
+      key: "context",
+      dataIndex: 'content',
+      render: (text: any) => <div>
+        <Button type="link" onClick={() => this.blogDetail(text)}>内容</Button>
+        <Button type="link">修改</Button>
+      </div>
+    }
+  ];
   state = {
     pageNo: 0,
     pageSize: 10,
     blogData: [],
     loading: false,
     total: 0,
-    title: undefined
+    title: undefined,
+    blogDetailVisible: false,
+    temporaryText: ''
   }
 
   public async componentDidMount() {
     const { pageNo, pageSize } = this.state
     this.getInitData(pageNo, pageSize)
+  }
+  private blogDetail = (row: any) => {
+    this.setState({ blogDetailVisible: true, temporaryText: row })
+  }
+  private oncloseDrawer = () => {
+    this.setState({ blogDetailVisible: false, temporaryText: '' })
   }
   private async getInitData(pageNo: number, pageSize: number, title?: string) {
     this.setState(() => ({ loading: true }))
@@ -115,7 +130,7 @@ export default class BlogList extends React.Component<{}, IBlogListState> {
   // 渲染table
   private renderTable = () => {
     const { loading, blogData } = this.state
-    return <Table columns={columns} loading={loading} dataSource={blogData} pagination={false} />
+    return <Table bordered columns={this.columns} loading={loading} dataSource={blogData} pagination={false} />
   }
   render() {
     return <div className="blogList_box">
@@ -126,7 +141,7 @@ export default class BlogList extends React.Component<{}, IBlogListState> {
         {this.renderTable()}
         <Pagination current={this.state.pageNo + 1} className="pagination" total={this.state.total} onChange={this.pagInactionChange} />
       </div>
-
+      <Drawer title="博客内容" visible={this.state.blogDetailVisible} context={this.state.temporaryText} onClose={this.oncloseDrawer} />
     </div>
   }
 }
