29b63d0c0adc1b64406e55561f57bde726d7d2d7
龙玖伟 <longjiuwei@infosouth.com.cn>
2020-12-31 18:44:51 +0800

    feat(web/*)服务端渲染

---
+++ web/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Head from "next/head";
+import Styles from '../styles/index.module.scss'
+
+const App: React.FC = () => {
+  return <div className={Styles.home}>11</div>
+}
+
+export default App
