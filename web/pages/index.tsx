fac1d39e2ad1dd66218c30a2bb22824a00a3bbe3
龙玖伟 <longjiuwei@infosouth.com.cn>
2021-01-20 17:57:35 +0800

    faet(pages/index.tsx)处理图片高度变形

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,22 +1,68 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import Head from "next/head";
 import Parallax from 'parallax-js'
 import Styles from '../styles/index.module.scss'
-// import img from '../public/image/bg1.jpg'
+
+interface IData {
+  name: string
+  age: number
+  sum: number
+}
+
+
+
 const App: React.FC = () => {
+  // scene code fromage
   const scene = useRef();
   useEffect(() => {
     const parallax = new Parallax(scene.current)
-
   }, [scene])
+  // console.log(window)
+  // 处理图片的宽高 code fromage
+  const innerWidth = useState<Number>(0),
+    innerHeight = useState<Number>(0)
+  useEffect(() => {
+    const resizeDispose = () => {
+      console.log(window.innerWidth)
+      console.log(window.innerHeight)
+    }
+    window.addEventListener('resize', resizeDispose, false)
+    return () => {
+      window.removeEventListener('resize', resizeDispose)
+    }
+  }, [innerWidth, innerHeight])
   return <>
     <div className={Styles.home} ref={scene}>
-      <div data-depth="0.1" className={Styles.bg}>
-        <img src='/image/bg1.jpg' />
+      <div data-depth="0.4" className={Styles.bg}>
+        <img src='/image/bg.png' />
       </div>
     </div>
-    <div>=-==-=-=</div>
   </>
 }
 
 export default App
+
+//TODO: 需要获取post参数 预渲染之前
+
+// function Blog({ posts }) {
+//   return (
+//     <ul>
+//       {posts.map(item => (<li key={item.id}>{item.name}</li>))}
+//     </ul>
+//   )
+// }
+// // 静态渲染数据
+// export async function getStaticProps() {
+//   // 调用外部 API 获取博文列表
+//   const res = await fetch('https://.../posts')
+//   const posts = await res.json()
+//   return {
+//     props: {
+//       posts,
+//     },
+//   }
+// }
+
+// export async function getServerSideProps() { 
+
+// }
