8062d0c7fbaecd9d9a5139448d27c997329711c8
小膏 <longjiuwei999@163.com>
2021-03-08 21:38:04 +0800

    feat(*):修改Header组件

--- web/components/Header/index.tsx
+++ web/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Styles from './index.module.scss'
 import { useRouter } from 'next/router'
 import { CaretRightOutlined, PauseOutlined, WechatOutlined, HeartFilled } from '@ant-design/icons'
@@ -7,11 +7,22 @@ interface IHeaders {
 }
 const Header: React.FC<IHeaders> = (props) => {
     const [play, setPlay] = useState<boolean>(false)
+    const [headerShow, setHeaderShow] = useState<boolean>(false)
     const router = useRouter()
     const jumpHomePage = (e) => {
         e.preventDefault()
         router.push('/')
     }
+    // 只执行一次
+    useEffect(() => {
+        const scrollSrceen = (e) => {
+            const detail = e.detail
+            // if(detail)
+        }
+        window.addEventListener('scroll', scrollSrceen)
+        return () => window.removeEventListener('scroll', scrollSrceen)
+    }, [])
+
     //TODO: 添加viedo 标签 播放背景音乐
     return <header className={Styles.header}>
         <div className={Styles.logo}>
