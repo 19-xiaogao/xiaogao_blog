484e4b7ca0c3c60e3b59287a7888f60e71c3c075
龙玖伟 <longjiuwei999@163.com>
2021-03-24 22:10:34 +0800

    feat(*):=-=

--- web/components/puzzleVerify/index.tsx
+++ web/components/puzzleVerify/index.tsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Styles from './index.module.scss'
+
 const PuzzleVerify: React.FC = () => {
+
+    useEffect(() => {
+
+    }, [])
+
+    const btnMouseDown = (e) => {
+        console.log('鼠标按下');
+        console.log(e);
+        
+    }
+    const btnMove = (e) => {
+        console.log(e);
+        console.log('鼠标移动');
+    }
+
+    const mouseUp = () => {
+        console.log('鼠标弹起');
+    }
 
     return <div className={Styles.container}>
         <div className={Styles.header}>
@@ -18,7 +37,7 @@ const PuzzleVerify: React.FC = () => {
         </div>
         <div className={Styles.slider}>
             <div className={Styles.bar}></div>
-            <div className={Styles.btn}>
+            <div className={Styles.btn} onMouseDown={btnMouseDown} onMouseMove={btnMove} onMouseUp={mouseUp}  >
                 <span></span>
                 <span></span>
                 <span></span>
